8c2da888bbd38f9276ab7500ab904397883132ac
Simiyu <simiyupaul415@gmail.com>
2023-06-13 08:52:04 -0700

    2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
-exports.converter = function (base) { return num => num.toString(base); };
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
+};
